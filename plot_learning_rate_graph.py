81d9fc18f928d02f75b799714618c39172966010
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-04-20 16:32:11 +0530

    added: graphs for diff learning rates

---
+++ plot_learning_rate_graph.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_json('result_learning_rate.json')
+df.columns = [0.2, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001]
+
+print(df.to_string())
+
+plt.figure()
+plt.title("Epoch vs Loss (MLP)")
+plt.xlabel("Epochs")
+plt.ylabel("Cross Entropy Loss")
+df.plot()
+plt.show()
