5847fbfe780fe9b947ac2e464fbc034a8523ac43
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-04-20 16:34:53 +0530

    updated: images for diff learning rates

--- plot_learning_rate_graph.py
+++ plot_learning_rate_graph.py
@@ -8,8 +8,10 @@ df.columns = [0.2, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001]
 print(df.to_string())
 
 plt.figure()
+df.iloc[:, 4:].plot()
+
 plt.title("Epoch vs Loss (MLP)")
 plt.xlabel("Epochs")
 plt.ylabel("Cross Entropy Loss")
-df.plot()
+
 plt.show()
